4632275957a8ef5afa5aaadc067a943751de790a
phamtienlong1820 <92737650+phamtienlong1820@users.noreply.github.com>
2021-12-17 12:17:14 +0700

    Delete adminpage directory

--- adminpage/layout/buttonbackpage.php
+++
@@ -1,3 +0,0 @@
-<div class="card-body" float="right">
-                <button type="button" class="btn btn-secondary " onclick="goBack()"> <i class="fa ti-arrow-circle-left"></i> Trở về</button>
-            </div>
