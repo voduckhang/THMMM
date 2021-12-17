25c058e53bb963c0d25e989e5ba27b1bf1f98fa7
phamtienlong1820 <92737650+phamtienlong1820@users.noreply.github.com>
2021-12-17 11:38:32 +0700

    Xoá file nggame
    Folder nggame chỉ trong nhánh userpage

--- nggame/view/chucnang/thanhsotrang/thanhsotrang-blog.php
+++
@@ -1,8 +0,0 @@
-<div class="col-lg-12">
-                            <div class="product__pagination blog__pagination">
-                                <a href="<?php include_once './chucnang/blog/blog.php'; ?>">1</a>
-                                <a href="<?php include_once './chucnang/blog/blog.php'; ?>">2</a>
-                                <a href="<?php include_once './chucnang/blog/blog.php'; ?>">3</a>
-                                <a href="<?php include_once './chucnang/blog/blog.php'; ?>"><i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
--- nggame/view/chucnang/thanhsotrang/thanhsotrang-shop-grid.php
+++
@@ -1,6 +0,0 @@
-<div class="product__pagination">
-                    <a href="<?php include_once './chucnang/sanpham/shop-grid.php'; ?>">1</a>
-                    <a href="<?php include_once './chucnang/sanpham/shop-grid.php'; ?>">2</a>
-                    <a href="<?php include_once './chucnang/sanpham/shop-grid.php'; ?>">3</a>
-                    <a href="<?php include_once './chucnang/sanpham/shop-grid.php'; ?>"><i class="fa fa-long-arrow-right"></i></a>
-                </div>
